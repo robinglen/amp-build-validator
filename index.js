61a1f4f816d53ef8b575dd82bdc86a7027ff9d05
thearegee <robin@iamrobinglen.com>
2017-02-23 11:18:19 +0000

    add in tool for valid markup from server

---
+++ index.js
@@ -0,0 +1,35 @@
+const rp = require('request-promise');
+const amphtmlValidator = require('amphtml-validator');
+
+const validator = {
+
+  init: (url) => {
+    rp(url)
+    .then((html) => {
+      amphtmlValidator.getInstance().then((validator) => {
+        const result = validator.validateString(html);
+        console.log(result);
+
+      }, (err) => {
+        console.log('AMP ERROR');
+        console.log(err);
+      })
+    }, (err) => {
+      console.log('REQUEST ERROR');
+      console.log(err);
+    })
+  },
+
+  ampResults: (results) => {
+    if (ValidationResult.status === 'FAIL') {
+      console.log(results)
+      // break build
+      process.exit(1);
+    } else {
+      console.log('GOOD TO GO')
+    }
+  }
+
+}
+
+exports.module = validator;
